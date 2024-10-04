ebc7783d2028e51bf16e1fe759fc3f834a44de4d
Hassan Aziz <47973@students.riphah.edu.pk>
2024-10-04 09:58:12 +0500

    implement lambda function

---
+++ src/Lambda/StringInterface.java
@@ -0,0 +1,5 @@
+package Lambda;
+
+public interface StringInterface {
+	String operate(String str);
+}
--- src/Lambda/TestInterface.java
+++ src/Lambda/TestInterface.java
@@ -2,5 +2,5 @@ package Lambda;
 
 @FunctionalInterface
 public interface TestInterface {
-	String printSomething();
+	int operate(int n1, int n2);
 }
